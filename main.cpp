eff7b0c1d8c17771f86898057e4883f77c7c75f8
Łukasz Musiał <lukasz.musial.tirek@gmail.com>
2018-06-24 12:38:51 +0200

    Implements Basic IO GPIO Operations

--- main.cpp
+++ main.cpp
@@ -11,26 +11,56 @@ NAN_METHOD(SetOut)
   pinMode(info[0]->NumberValue(), OUTPUT);
 }
 
+NAN_METHOD(SetIn)
+{
+  pinMode(info[0]->NumberValue(), INPUT);
+}
+
 NAN_METHOD(On)
 {
   digitalWrite(info[0]->NumberValue(), HIGH);
-
-  info.GetReturnValue().Set(Nan::Undefined());
 }
 
 NAN_METHOD(Off)
 {
   digitalWrite(info[0]->NumberValue(), LOW);
+}
 
-  info.GetReturnValue().Set(Nan::Undefined());
+NAN_METHOD(In)
+{
+  info.GetReturnValue().Set(digitalRead(info[0]->NumberValue()));
+}
+
+NAN_METHOD(PullUp)
+{
+  pullUpDnControl(info[0]->NumberValue(), PUD_UP);
+}
+
+NAN_METHOD(PullDown)
+{
+  pullUpDnControl(info[0]->NumberValue(), PUD_DOWN);
+}
+
+NAN_METHOD(PullOff)
+{
+  pullUpDnControl(info[0]->NumberValue(), PUD_OFF);
 }
 
 NAN_MODULE_INIT(InitModule)
 {
   NAN_EXPORT(target, Init);
+
   NAN_EXPORT(target, SetOut);
+  NAN_EXPORT(target, SetIn);
+
   NAN_EXPORT(target, On);
   NAN_EXPORT(target, Off);
+
+  NAN_EXPORT(target, In);
+
+  NAN_EXPORT(target, PullUp);
+  NAN_EXPORT(target, PullDown);
+  NAN_EXPORT(target, PullOff);
 }
 
 NODE_MODULE(GPIO, InitModule);
