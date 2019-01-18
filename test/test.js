4220a1ed6c286de454944d98cd9c9958f09a0ddd
musialny.dev <musialny.dev@gmail.com>
2019-01-18 22:49:24 +0100

    Test update

--- test/test.js
+++ test/test.js
@@ -8,20 +8,26 @@ GPIO.SetPin(23, PIN.MODE.PWM_OUTPUT);
 
 GPIO.PullControl(31, PIN.MODE.PULL_UP);
 
-if (GPIO.Read(31)) {
-    GPIO.Write(27, PIN.MODE.HIGH);
-} else if (!GPIO.Read(31)) {
-    GPIO.Write(27, PIN.MODE.LOW);
+function checkSetLedState() {
+    if (GPIO.Read(31)) {
+        GPIO.Write(27, PIN.MODE.HIGH);
+    } else if (!GPIO.Read(31)) {
+        GPIO.Write(27, PIN.MODE.LOW);
+    }
 }
 
 for (let o = 0; o < 5; o++) {
     for (let i = 0; i < 1025; i++) {
+        checkSetLedState();
+
         GPIO.PWM(23, i);
         console.log(i);
         GPIO.Wait(1);
     }
 
     for (let i = 1024; i >= 0; i--) {
+        checkSetLedState();
+
         GPIO.PWM(23, i);
         console.log(i);
         GPIO.Wait(1);
